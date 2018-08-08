bde54929902fe0cdcfa896ff71241c2db1c5e92b
alexandra-leveille <you@example.com>
2018-08-08 15:20:52 +0200

    version6

--- controllers/command.js
+++ controllers/command.js
@@ -1,4 +1,4 @@
-//jshit esversion:6
+//jshint esversion:6
 console.log('controllers/command');
 
 const express = require('express')
@@ -6,8 +6,26 @@ const model = require('../models/command')
 
 module.exports = express.Router()
 .get('/', (req, res) => {
-  model.getCommand(
+  model.getCommand()
     .then(result => res.json(result))
     .catch(err => res.json(err))
   })
-  .post('/', (req, res) =>)
+  .post('/', (req, res) =>{
+    const { name, lieux } = req.body;
+    model.createCommand({ name, lieux })
+    .then(result => res.send(result))
+    .catch(err => console.log(err))
+  })
+  .put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, lieux } = req.body;
+    model.updateCommand({ id, name, lieux })
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+  })
+  .delete('/:id', (req, res) => {
+    const { id } = req.params;
+    model.deleteCommand(id)
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+  })
---
+++ models/command.js
@@ -0,0 +1,21 @@
+// jshint esversion:6
+const db = require ('./db')
+module.exports = {
+  getCommand(){
+    return db.query('SELECT * FROM command ORDER BY id')
+  },
+  createCommand({ name, lieux }) {
+    return db.query(`
+      INSERT INTO command (name, lieux)
+      VALUES ('${name}', '${lieux}')`)
+  },
+  updateCommand({ id, name, lieux }){
+    return db.query(`
+      UPDATE command
+      SET name='${name}', lieux='${lieux}'
+      WHERE id=${id} `)
+  },
+  deleteCommand(id){
+    return db.query(`DELETE FROM command WHERE id=${id}`)
+  }
+}
